c7879104448972320d0617b05d3f9ab604b21683
Saturn-VI <112597058+Saturn-VI@users.noreply.github.com>
2026-01-31 15:07:07 -0600

    change tab size & add logging to shooter

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -19,250 +19,251 @@ import frc.robot.subsystems.drive.DriveSubsystem;
 
 public class ShooterSubsystem extends SubsystemBase implements AutoCloseable {
 
-    private static ShooterSubsystem s_shooterSubsystem;
-    private TalonFX m_shooterMotorMaster;
-    private TalonFX m_shooterMotorSlaveOne;
-    private TalonFX m_shooterMotorSlaveTwo;
-    private TalonFX m_indexerMotor;
-    private TalonFX m_hoodMotor;
+  private static ShooterSubsystem s_shooterSubsystem;
+  private TalonFX m_shooterMotorMaster;
+  private TalonFX m_shooterMotorSlaveOne;
+  private TalonFX m_shooterMotorSlaveTwo;
+  private TalonFX m_indexerMotor;
+  private TalonFX m_hoodMotor;
 
-    private final MotionMagicVelocityVoltage m_shooterRequest;
-    private final MotionMagicVelocityVoltage m_indexerRequest;
-    private final MotionMagicVoltage m_hoodRequest;
+  private final MotionMagicVelocityVoltage m_shooterRequest;
+  private final MotionMagicVelocityVoltage m_indexerRequest;
+  private final MotionMagicVoltage m_hoodRequest;
 
-    public static ShooterSubsystem getInstance() {
-        if (s_shooterSubsystem == null) {
-            s_shooterSubsystem = new ShooterSubsystem();
-        }
-        return s_shooterSubsystem;
+  public static ShooterSubsystem getInstance() {
+    if (s_shooterSubsystem == null) {
+      s_shooterSubsystem = new ShooterSubsystem();
     }
+    return s_shooterSubsystem;
+  }
 
-    private ShooterSubsystem() {
-        m_shooterMotorMaster = new TalonFX(Constants.Shooter.MASTER_SHOOTER_MOTOR_ID);
-        m_shooterMotorMaster = new TalonFX(Constants.Shooter.SLAVE_SHOOTER_ONE_MOTOR_ID);
-        m_shooterMotorMaster = new TalonFX(Constants.Shooter.SLAVE_SHOOTER_TWO_MOTOR_ID);
-        m_indexerMotor = new TalonFX(Constants.Shooter.INDEXER_MOTOR_ID);
-        m_hoodMotor = new TalonFX(Constants.Shooter.HOOD_MOTOR_ID);
+  private ShooterSubsystem() {
+    m_shooterMotorMaster = new TalonFX(Constants.Shooter.MASTER_SHOOTER_MOTOR_ID);
+    m_shooterMotorMaster = new TalonFX(Constants.Shooter.SLAVE_SHOOTER_ONE_MOTOR_ID);
+    m_shooterMotorMaster = new TalonFX(Constants.Shooter.SLAVE_SHOOTER_TWO_MOTOR_ID);
+    m_indexerMotor = new TalonFX(Constants.Shooter.INDEXER_MOTOR_ID);
+    m_hoodMotor = new TalonFX(Constants.Shooter.HOOD_MOTOR_ID);
 
-        m_shooterRequest = new MotionMagicVelocityVoltage(0);
-        m_indexerRequest = new MotionMagicVelocityVoltage(0);
-        m_hoodRequest = new MotionMagicVoltage(0);
+    m_shooterRequest = new MotionMagicVelocityVoltage(0);
+    m_indexerRequest = new MotionMagicVelocityVoltage(0);
+    m_hoodRequest = new MotionMagicVoltage(0);
 
-        // TODO set up configs
-        // for reference:
-        // https://github.com/lasarobotics/PH2025/blob/master/src/main/java/frc/robot/subsystems/lift/LiftSubsystem.java#L1359-L1422
-        TalonFXConfiguration shooterOneConfig = new TalonFXConfiguration();
-        TalonFXConfiguration shooterTwoConfig = new TalonFXConfiguration();
-        TalonFXConfiguration shooterThreeConfig = new TalonFXConfiguration();
+    // TODO set up configs
+    // for reference:
+    // https://github.com/lasarobotics/PH2025/blob/master/src/main/java/frc/robot/subsystems/lift/LiftSubsystem.java#L1359-L1422
+    TalonFXConfiguration shooterOneConfig = new TalonFXConfiguration();
+    TalonFXConfiguration shooterTwoConfig = new TalonFXConfiguration();
+    TalonFXConfiguration shooterThreeConfig = new TalonFXConfiguration();
 
-        TalonFXConfiguration indexerConfig = new TalonFXConfiguration();
+    TalonFXConfiguration indexerConfig = new TalonFXConfiguration();
 
-        TalonFXConfiguration hoodConfig = new TalonFXConfiguration();
+    TalonFXConfiguration hoodConfig = new TalonFXConfiguration();
 
-        m_shooterMotorMaster.getConfigurator().apply(shooterOneConfig);
-        m_shooterMotorSlaveOne.getConfigurator().apply(shooterTwoConfig);
-        m_shooterMotorSlaveTwo.getConfigurator().apply(shooterThreeConfig);
-        m_indexerMotor.getConfigurator().apply(indexerConfig);
-        m_hoodMotor.getConfigurator().apply(hoodConfig);
-    }
+    m_shooterMotorMaster.getConfigurator().apply(shooterOneConfig);
+    m_shooterMotorSlaveOne.getConfigurator().apply(shooterTwoConfig);
+    m_shooterMotorSlaveTwo.getConfigurator().apply(shooterThreeConfig);
+    m_indexerMotor.getConfigurator().apply(indexerConfig);
+    m_hoodMotor.getConfigurator().apply(hoodConfig);
+  }
 
-    /**
-     * Periodic shooter logic. Basically:
-     * <ul>
-     * <li>Always adjust hood
-     * <li>If holding shoot/pass button:
-     * <ul>
-         * <li>Set shoot motor to shoot speed
-         * <li>Toggle indexer motor based on if ready to shoot/pass
-     * </ul>
-     * <li>If not holding button, set shoot motor to hold speed
-     * and stop indexer
-     * </ul
-     * @param shooting If shoot button is held
-     * @param passing If pass button is held
-     */
-    public void shooterPeriodic(boolean shooting, boolean passing) {
-        adjustHood();
-        if (shooting || passing) {
-            shoot();
-            if ((readyToShoot() && shooting) ||
-                (readyToPass() && passing)) {
-                index();
-            } else {
-                stopIndexer();
-            }
-        } else {
-            holdShooter();
-            stopIndexer();
-        }
-    }
-
-    /**
-     * Stops all motors.
-     */
-    public void stopEverything() {
-        stopShooter();
+  /**
+   * Periodic shooter logic. Basically:
+   * <ul>
+   * <li>Always adjust hood
+   * <li>If holding shoot/pass button:
+   * <ul>
+     * <li>Set shoot motor to shoot speed
+     * <li>Toggle indexer motor based on if ready to shoot/pass
+   * </ul>
+   * <li>If not holding button, set shoot motor to hold speed
+   * and stop indexer
+   * </ul
+   * @param shooting If shoot button is held
+   * @param passing If pass button is held
+   */
+  public void shooterPeriodic(boolean shooting, boolean passing) {
+    adjustHood();
+    if (shooting || passing) {
+      shoot();
+      if ((readyToShoot() && shooting) ||
+        (readyToPass() && passing)) {
+        index();
+      } else {
         stopIndexer();
-        stopHood();
+      }
+    } else {
+      holdShooter();
+      stopIndexer();
     }
+  }
 
-    /**
-     * Stop the {@link #m_indexerMotor indexer motor}.
-     */
-    public void stopIndexer() {
-        m_indexerMotor.setControl(
-            m_indexerRequest.withVelocity(0)
-        );
-    }
+  /**
+   * Stops all motors.
+   */
+  public void stopEverything() {
+    stopShooter();
+    stopIndexer();
+    stopHood();
+  }
 
-    /**
-     * Set the speed of the {@link #m_indexerMotor indexer motor}
-     * to the (constant)
-     * {@link Constants.Shooter#indexerHoldSpeed indexer hold speed}.
-     */
-    public void index() {
-        m_indexerMotor.setControl(
-            m_indexerRequest.withVelocity(Constants.Shooter.INDEXER_MOTOR_SPEED)
-        );
-    }
+  /**
+   * Stop the {@link #m_indexerMotor indexer motor}.
+   */
+  public void stopIndexer() {
+    m_indexerMotor.setControl(
+      m_indexerRequest.withVelocity(0)
+    );
+  }
 
-    /**
-     * Stop the {@link #m_shooterMotorMaster shooter motors} (coast to 0).
-     */
-    public void stopShooter() {
-        m_shooterMotorMaster.setVoltage(0);
-        m_shooterMotorSlaveOne.setVoltage(0);
-        m_shooterMotorSlaveTwo.setVoltage(0);
-    }
+  /**
+   * Set the speed of the {@link #m_indexerMotor indexer motor}
+   * to the (constant)
+   * {@link Constants.Shooter#indexerHoldSpeed indexer hold speed}.
+   */
+  public void index() {
+    m_indexerMotor.setControl(
+      m_indexerRequest.withVelocity(Constants.Shooter.INDEXER_MOTOR_SPEED)
+    );
+  }
 
-    /**
-     * Set the speed of one {@link #m_shooterMotorMaster shooter motor}
-     * to the (constant)
-     * {@link Constants.Shooter#SHOOTER_HOLD_SPEED shooter hold speed}
-     * and let the others coast.
-     */
-    public void holdShooter() {
-        m_shooterMotorMaster.setControl(
-            m_shooterRequest.withVelocity(Constants.Shooter.SHOOTER_HOLD_SPEED)
-        );
-        m_shooterMotorSlaveOne.setVoltage(0);
-        m_shooterMotorSlaveTwo.setVoltage(0);
-    }
+  /**
+   * Stop the {@link #m_shooterMotorMaster shooter motors} (coast to 0).
+   */
+  public void stopShooter() {
+    m_shooterMotorMaster.setVoltage(0);
+    m_shooterMotorSlaveOne.setVoltage(0);
+    m_shooterMotorSlaveTwo.setVoltage(0);
+  }
 
-    /**
-     * Set the speed of the {@link #m_shooterMotorMaster shooter motors}
-     * to the desired shooting speed
-     * according to {@link #wantedShooterSpeed()}
-     */
-    public void shoot() {
-        m_shooterMotorMaster.setControl(
-            m_shooterRequest.withVelocity(wantedShooterSpeed())
-        );
-        m_shooterMotorSlaveOne.setControl(
-            m_shooterRequest.withVelocity(wantedShooterSpeed())
-        );
-        m_shooterMotorSlaveTwo.setControl(
-            m_shooterRequest.withVelocity(wantedShooterSpeed())
-        );
-    }
+  /**
+   * Set the speed of one {@link #m_shooterMotorMaster shooter motor}
+   * to the (constant)
+   * {@link Constants.Shooter#SHOOTER_HOLD_SPEED shooter hold speed}
+   * and let the others coast.
+   */
+  public void holdShooter() {
+    m_shooterMotorMaster.setControl(
+      m_shooterRequest.withVelocity(Constants.Shooter.SHOOTER_HOLD_SPEED)
+    );
+    m_shooterMotorSlaveOne.setVoltage(0);
+    m_shooterMotorSlaveTwo.setVoltage(0);
+  }
 
-    /**
-     * 
-     * @return If the master shooter motor is {@link Constants.Shooter#SHOOTER_SPEED_TOLERANCE near}
-     * the desired speed according to {@link #wantedShooterSpeed()}
-     */
-    public boolean atShootSpeed() {
-        return m_shooterMotorMaster.getVelocity().isNear(
-            wantedShooterSpeed(),
-            Constants.Shooter.SHOOTER_SPEED_TOLERANCE
-        );
-    }
+  /**
+   * Set the speed of the {@link #m_shooterMotorMaster shooter motors}
+   * to the desired shooting speed
+   * according to {@link #wantedShooterSpeed()}
+   */
+  public void shoot() {
+    m_shooterMotorMaster.setControl(
+      m_shooterRequest.withVelocity(wantedShooterSpeed())
+    );
+    m_shooterMotorSlaveOne.setControl(
+      m_shooterRequest.withVelocity(wantedShooterSpeed())
+    );
+    m_shooterMotorSlaveTwo.setControl(
+      m_shooterRequest.withVelocity(wantedShooterSpeed())
+    );
+  }
 
-    /**
-     * Sets the target position of the hood to the current hood position
-     * (i.e. stop the hood)
-     */
-    public void stopHood() {
-        double pos = m_hoodMotor.getPosition().getValueAsDouble();
-        m_hoodMotor.setControl(m_hoodRequest.withPosition(pos));
-    }
+  /**
+   * 
+   * @return If the master shooter motor is {@link Constants.Shooter#SHOOTER_SPEED_TOLERANCE near}
+   * the desired speed according to {@link #wantedShooterSpeed()}
+   */
+  public boolean atShootSpeed() {
+    return m_shooterMotorMaster.getVelocity().isNear(
+      wantedShooterSpeed(),
+      Constants.Shooter.SHOOTER_SPEED_TOLERANCE
+    );
+  }
 
-    /**
-     * Set the setpoint of the {@link #m_hoodMotor hood motor}
-     * to the desired hood position
-     * according to {@link #wantedHoodPosition()}
-     */
-    public void adjustHood() {
-        m_hoodMotor.setControl(
-            m_hoodRequest.withPosition(wantedHoodPosition())
-        );
-    }
+  /**
+   * Sets the target position of the hood to the current hood position
+   * (i.e. stop the hood)
+   */
+  public void stopHood() {
+    double pos = m_hoodMotor.getPosition().getValueAsDouble();
+    m_hoodMotor.setControl(m_hoodRequest.withPosition(pos));
+  }
 
-    /**
-     * Checks that the robot can make it in if it shoots right now
-     * and that the drivetrain is at the wanted rotation
-     * @return If robot is in a good position to shoot
-     */
-    public boolean readyToShoot() {
-        return (
-            (
-                GameHelpers.scoringTimeLeft() - Constants.Drive.HANG_TIME
-                >= Constants.Shooter.SHOOTER_TIME_MARGIN
-            ) &&
-            atShootSpeed() &&
-            DriveSubsystem.getInstance().atWantedRotation()
-        );
-    }
+  /**
+   * Set the setpoint of the {@link #m_hoodMotor hood motor}
+   * to the desired hood position
+   * according to {@link #wantedHoodPosition()}
+   */
+  public void adjustHood() {
+    m_hoodMotor.setControl(
+      m_hoodRequest.withPosition(wantedHoodPosition())
+    );
+  }
 
-    /**
-     * Checks that the robot can make it in the alliance zone if it shoots right now
-     * and that the drivetrain is at the wanted rotation
-     * (doesn't check if hub is active)
-     * @return If robot is in a good position to shoot
-     */
-    public boolean readyToPass() {
-        return atShootSpeed() && DriveSubsystem.getInstance().atWantedRotation();
-    }
+  /**
+   * Checks that the robot can make it in if it shoots right now
+   * and that the drivetrain is at the wanted rotation
+   * @return If robot is in a good position to shoot
+   */
+  public boolean readyToShoot() {
+    return (
+      (
+        GameHelpers.scoringTimeLeft() - Constants.Drive.HANG_TIME
+        >= Constants.Shooter.SHOOTER_TIME_MARGIN
+      ) &&
+      atShootSpeed() &&
+      DriveSubsystem.getInstance().atWantedRotation()
+    );
+  }
 
-    /**
-     * Get the current wanted ball velocity from AimUtil
-     * and convert to rotations per second.
-     * @return The wanted rotations per second
-     */
-    public double wantedShooterSpeed() {
-        LinearVelocity ballVelocity = AimUtil.getBallVelocity();
-        double radiansPerSecond =
-            2 * ballVelocity.in(MetersPerSecond)
-            / Constants.Shooter.SHOOTER_RADIUS.in(Meters);
-        double rotationsPerSecond =
-            radiansPerSecond / (2 * Math.PI);
-        return rotationsPerSecond;
-    }
+  /**
+   * Checks that the robot can make it in the alliance zone if it shoots right now
+   * and that the drivetrain is at the wanted rotation
+   * (doesn't check if hub is active)
+   * @return If robot is in a good position to shoot
+   */
+  public boolean readyToPass() {
+    return atShootSpeed() && DriveSubsystem.getInstance().atWantedRotation();
+  }
 
-    // TODO implement
-    // return a number of rotations
-    public double wantedHoodPosition() {
-        Angle theta = AimUtil.getExitAngle();
-        return 0;
-    }
+  /**
+   * Get the current wanted ball velocity from AimUtil
+   * and convert to rotations per second.
+   * @return The wanted rotations per second
+   */
+  public double wantedShooterSpeed() {
+    LinearVelocity ballVelocity = AimUtil.getBallVelocity();
+    double radiansPerSecond =
+      2 * ballVelocity.in(MetersPerSecond)
+      / Constants.Shooter.SHOOTER_RADIUS.in(Meters);
+    double rotationsPerSecond =
+      radiansPerSecond / (2 * Math.PI);
+    return rotationsPerSecond;
+  }
 
-    /**
-     * Converts a given angle to the number of rotations
-     * the hood motor would be at at that angle
-     * @param angle The angle to convert
-     * @return The number of rotations
-     */
-    public double hoodAngleToRotations(Angle angle) {
-        double base = angle.in(Rotations);
-        double rotations = base * Constants.Shooter.HOOD_TO_MOTOR_RATIO;
-        return rotations;
-    }
+  // TODO implement
+  // return a number of rotations
+  public double wantedHoodPosition() {
+    Angle theta = AimUtil.getExitAngle();
+    return 0;
+  }
 
-    public void close() {
-        m_shooterMotorMaster.close();
-        m_shooterMotorSlaveOne.close();
-        m_shooterMotorSlaveTwo.close();
-        m_indexerMotor.close();
-        m_hoodMotor.close();
-    }
+  /**
+   * Converts a given angle to the number of rotations
+   * the hood motor would be at at that angle
+   * @param angle The angle to convert
+   * @return The number of rotations
+   */
+  public double hoodAngleToRotations(Angle angle) {
+    double base = angle.in(Rotations);
+    double rotations = base * Constants.Shooter.HOOD_TO_MOTOR_RATIO;
+    return rotations;
+  }
+
+  @Override
+  public void close() {
+    m_shooterMotorMaster.close();
+    m_shooterMotorSlaveOne.close();
+    m_shooterMotorSlaveTwo.close();
+    m_indexerMotor.close();
+    m_hoodMotor.close();
+  }
 }
