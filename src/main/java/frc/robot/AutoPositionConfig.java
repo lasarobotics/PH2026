3fa72a410bcdd2a4c481abd9634d1dab6e6b5290
Saturn-VI <112597058+Saturn-VI@users.noreply.github.com>
2026-02-23 19:42:02 -0600

    organize imports

--- src/main/java/frc/robot/AutoPositionConfig.java
+++ src/main/java/frc/robot/AutoPositionConfig.java
@@ -1,8 +1,6 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 public class AutoPositionConfig {
