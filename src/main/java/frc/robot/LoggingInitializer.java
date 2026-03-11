f96ce16132d59344185f6bc5d88811215c0cdd84
Saturn-VI <maxwang420@gmail.com>
2026-03-10 21:15:18 -0500

    state machine version, kinda broken

---
+++ src/main/java/frc/robot/LoggingInitializer.java
@@ -0,0 +1,226 @@
+package frc.robot;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+import org.lasarobotics.fsm.StateMachine;
+import org.lasarobotics.fsm.SystemState;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import com.ctre.phoenix6.SignalLogger;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.MatchType;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+
+/**
+ * Most of this code is a carbon copy of the logic in
+ * {@link org.littletonrobotics.junction.wpilog.WPILOGWriter WPILOGWriter}
+ */
+public class LoggingInitializer extends StateMachine {
+
+  public enum LoggingStates implements SystemState {
+    START {
+      @Override
+      public void execute() {
+        if (
+          (
+            DriverStation.isDSAttached() &&
+            HAL.getSystemTimeValid()
+          ) ||
+          RobotBase.isSimulation()
+        ) {
+          if (dsAttachedTime == null) {
+            dsAttachedTime = RobotController.getFPGATime() / 1000000.0;
+          }
+          
+          // Set logDate if we've waited long enough OR if we are in simulation
+          if (logDate == null) {
+            if (RobotBase.isSimulation() || 
+                (RobotController.getFPGATime() / 1000000.0 - dsAttachedTime > timestampUpdateDelay)) {
+              logDate = LocalDateTime.now();
+            }
+          }
+        } else {
+          dsAttachedTime = null;
+        }
+
+        MatchType matchType = DriverStation.getMatchType();
+        if (logMatchText == null && matchType != MatchType.None) {
+          logMatchText = "";
+          switch (matchType) {
+            case Practice:
+              logMatchText = "p";
+              break;
+            case Qualification:
+              logMatchText = "q";
+              break;
+            case Elimination:
+              logMatchText = "e";
+              break;
+            default:
+              break;
+          }
+          logMatchText += Integer.toString(DriverStation.getMatchNumber());
+        }
+
+        // Update folder
+        StringBuilder folderNameBuilder = new StringBuilder();
+        folderNameBuilder.append("418_");
+        // something like 01-01-01 or abc123
+        if (logDate == null) {
+          folderNameBuilder.append(randomIdentifier);
+        } else {
+          folderNameBuilder.append(timeFormatter.format(logDate));
+        }
+        String eventName = DriverStation.getEventName().toLowerCase();
+        // if event, abc123_txbel
+        if (eventName.length() > 0) {
+          folderNameBuilder.append("_");
+          folderNameBuilder.append(eventName);
+        }
+        // if match type/number, abc123_txbel_q44
+        if (logMatchText != null) {
+          folderNameBuilder.append("_");
+          folderNameBuilder.append(logMatchText);
+        }
+        folder = basePath.resolve(folderNameBuilder.toString());
+      }
+
+      @Override
+      public SystemState nextState() {
+        // there's an extra 0.5 second delay on top of the 5 seconds
+        // so the robot has time to get things like match type/number/time/etc
+        if (
+          RobotBase.isSimulation() ||
+          (
+            dsAttachedTime != null &&
+            RobotController.getFPGATime() / 1000000.0 - dsAttachedTime
+              > timestampUpdateDelay + 0.5
+          )
+        ) return LOGGING;
+
+        return this;
+      }
+    },
+    LOGGING {
+      @Override
+      public void initialize() {
+        try {
+          Files.createDirectories(folder);
+        } catch (IOException e) {
+          // probably won't happen
+          // TODO figure out what to do here (if anything)
+          e.printStackTrace();
+        }
+        
+        // advantagekit
+        Logger.addDataReceiver(new WPILOGWriter(
+          folder.resolve("akit_log.wpilog").toString()
+        ));
+        Logger.addDataReceiver(new NT4Publisher());
+
+        Logger.start();
+
+        // ctre
+        SignalLogger.setPath(folder.toString());
+        SignalLogger.start();
+      }
+
+      @Override
+      public SystemState nextState() {
+        // Logger.recordOutput("LoggingInitializer/condition1", (getInstance().teleopEnded &&
+        //     DriverStation.getMatchType() != MatchType.None));
+        // Logger.recordOutput("LoggingInitializer/condition2", shouldStopLogging.get());
+        // basically if it's a real match and teleop is over
+        // it's probably the end of the match
+        if (
+          (
+            getInstance().teleopEnded &&
+            DriverStation.getMatchType() != MatchType.None
+          ) // ||
+          // shouldStopLogging.get()
+        ) {
+          return END;
+        }
+
+        return this;
+      }
+    },
+    END {
+      @Override
+      public void initialize() {
+        if (DriverStation.getMatchType() != MatchType.None) {
+          // 165 is the max
+          // 140 sec teleop + 3 sec delay + 20 sec auto = 163
+          // so might as well get all of it
+          LimelightHelpers.triggerRewindCapture(Constants.Drive.SHOOTER_LIMELIGHT_NAME, 165);
+          LimelightHelpers.triggerRewindCapture(Constants.Drive.CLIMB_LIMELIGHT_NAME, 165);
+        }
+      }
+
+      @Override
+      public SystemState nextState() {
+        return this;
+      }
+    }
+  }
+  
+  private static LoggingInitializer s_loggingInitializer;
+
+  private static final double timestampUpdateDelay =
+    5.0; // Wait several seconds after DS attached to ensure
+         // timestamp/timezone is updated
+  private static final Path defaultPathRio = Path.of("/U/logs");
+  private static final Path defaultPathSim = Path.of("logs");
+  private static final DateTimeFormatter timeFormatter =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+
+  private static Path basePath;
+  private static Path folder;
+  private static String randomIdentifier;
+  private static Double dsAttachedTime;
+
+  private static LocalDateTime logDate;
+  private static String logMatchText;
+
+  private boolean teleopEnded = false;
+
+  public static LoggingInitializer getInstance() {
+    if (s_loggingInitializer == null) {
+      s_loggingInitializer = new LoggingInitializer();
+    }
+    return s_loggingInitializer;
+  }
+
+  public LoggingInitializer() {
+    super(LoggingStates.START);
+    basePath = RobotBase.isSimulation() ? defaultPathSim : defaultPathRio;
+    folder = basePath; // avoid nullpointerexception
+
+    Random random = new Random();
+    StringBuilder randomIdentifierBuilder = new StringBuilder();
+    for (int i = 0; i < 4; i++) {
+      randomIdentifierBuilder.append(String.format("%04x", random.nextInt(0x10000)));
+    }
+    randomIdentifier = randomIdentifierBuilder.toString();
+  }
+
+  @Override
+  public void periodic() {
+    Logger.recordOutput(getName() + "/state", getState().toString());
+  }
+
+  public void stopLogging() {
+    teleopEnded = true;
+  }
+}
