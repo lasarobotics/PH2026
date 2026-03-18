257a3d59985ad4f94f400ccf400190ecf5ae27c2
ArjunS <writetoarjun1@gmail.com>
2026-03-18 17:58:16 -0500

    Correct logging

--- src/main/java/frc/robot/VisionUtil.java
+++ src/main/java/frc/robot/VisionUtil.java
@@ -7,12 +7,17 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 public class VisionUtil {
   // TODO: Change to real camera name in photon vision UI
   private static final String CAMERA_NAME = "Arducam_OV9782_USB_CAMERA";
   private static final String LOG_PATH = "VisionUtil";
   private static final int INVALID_FIDUCIAL_ID = -1;
   private static final double CLEARED_DOUBLE_VALUE = 0.0;
+  private static final Transform3d CLEARED_POSE = new Transform3d();
+  private static final Translation3d CLEARED_POS = new Translation3d();
 
   private static final PhotonCamera s_camera = new PhotonCamera(CAMERA_NAME);
   private static PhotonPipelineResult s_latestResult = s_camera.getLatestResult();
@@ -26,30 +31,6 @@ public class VisionUtil {
 
   public static int getObjectCount() {
     return s_latestResult == null ? 0 : s_latestResult.getTargets().size();
-  }
-
-  public static double getBestObjectYaw() {
-    if (!hasObjects()) {
-      return CLEARED_DOUBLE_VALUE;
-    }
-
-    return s_latestResult.getBestTarget().getYaw();
-  }
-
-  public static double getBestObjectPitch() {
-    if (!hasObjects()) {
-      return CLEARED_DOUBLE_VALUE;
-    }
-
-    return s_latestResult.getBestTarget().getPitch();
-  }
-
-  public static double getBestObjectArea() {
-    if (!hasObjects()) {
-      return CLEARED_DOUBLE_VALUE;
-    }
-
-    return s_latestResult.getBestTarget().getArea();
   }
 
   public static void updateVisionData() {
@@ -68,15 +49,11 @@ public class VisionUtil {
       // logs best object according to Photon Vision, basicaly biggest bounding box
       PhotonTrackedTarget bestObject = s_latestResult.getBestTarget();
       Logger.recordOutput(LOG_PATH + "/BestObject/id", bestObject.getFiducialId());
-      Logger.recordOutput(LOG_PATH + "/BestObject/yaw", bestObject.getYaw());
-      Logger.recordOutput(LOG_PATH + "/BestObject/pitch", bestObject.getPitch());
-      Logger.recordOutput(LOG_PATH + "/BestObject/area", bestObject.getArea());
+      Logger.recordOutput(LOG_PATH + "/BestObject/pose", bestObject.getBestCameraToTarget());
     } else {
       // clears best object logs when nothing is visible
       Logger.recordOutput(LOG_PATH + "/BestObject/id", INVALID_FIDUCIAL_ID);
-      Logger.recordOutput(LOG_PATH + "/BestObject/yaw", CLEARED_DOUBLE_VALUE);
-      Logger.recordOutput(LOG_PATH + "/BestObject/pitch", CLEARED_DOUBLE_VALUE);
-      Logger.recordOutput(LOG_PATH + "/BestObject/area", CLEARED_DOUBLE_VALUE);
+      Logger.recordOutput(LOG_PATH + "/BestObject/pose", CLEARED_POSE);
     }
 
     // logs every object currently seen
@@ -84,20 +61,14 @@ public class VisionUtil {
       PhotonTrackedTarget object = objects.get(i);
       String objectPath = LOG_PATH + "/" + (i + 1) + "_Object";
       Logger.recordOutput(objectPath + "/id", object.getFiducialId());
-      Logger.recordOutput(objectPath + "/yaw", object.getYaw());
-      Logger.recordOutput(objectPath + "/pitch", object.getPitch());
-      Logger.recordOutput(objectPath + "/area", object.getArea());
-      Logger.recordOutput(objectPath + "/skew", object.getSkew());
+      Logger.recordOutput(objectPath + "/pose", object.getBestCameraToTarget().getTranslation());
     }
 
     // clears old objects if those objects were lost since last loop
     for (int i = objects.size(); i < s_lastObjectCount; i++) {
       String objectPath = LOG_PATH + "/" + (i + 1) + "_Object";
       Logger.recordOutput(objectPath + "/id", INVALID_FIDUCIAL_ID);
-      Logger.recordOutput(objectPath + "/yaw", CLEARED_DOUBLE_VALUE);
-      Logger.recordOutput(objectPath + "/pitch", CLEARED_DOUBLE_VALUE);
-      Logger.recordOutput(objectPath + "/area", CLEARED_DOUBLE_VALUE);
-      Logger.recordOutput(objectPath + "/skew", CLEARED_DOUBLE_VALUE);
+      Logger.recordOutput(objectPath + "/pose", CLEARED_POS);
     }
 
     s_lastObjectCount = objects.size();
